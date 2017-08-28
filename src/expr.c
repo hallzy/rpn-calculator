776f469c224c18966cb92c6f24f0ae16ef8d7de6
Steven Hall <Hallzy.18@gmail.com>
2017-08-28 01:38:00 -0700

    Added a bunch of comments to expr.c

--- src/expr.c
+++ src/expr.c
@@ -6,8 +6,17 @@
 // Parses a 1 line RPN expression
 ret_codes parseExpression(char *val) {
   char *p;
+  // strtok searches my string (first arg) for the delimiters (arg 2). When it
+  // has found one it gives me the string that I want in p.
+  // Example: if val = "This is a test", p will be "This", then "is", then "a",
+  // and then finally "test"
+  // NOTE: the delimiters can be a list of delimiters such as " ,.-". Currently
+  // I do not need this but it is something to consider in the future if I ever
+  // have the need for it.
   p = strtok (val, " ");
+  // As soon as p becomes null, we have reached the end of our string.
   while (p != NULL) {
+    // For every result, send it to get processed
     processUserInput(p, strlen(p));
     // Calling this with null starts parsing the string where it left off the
     // last time it ran.
