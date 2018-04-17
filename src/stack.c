4bc0e9d7adb81844587d7dbc64780e04b36414ec
Steven Hall <Hallzy.18@gmail.com>
2018-04-17 01:48:05 -0700

    Fixed reduced scope error

--- src/stack.c
+++ src/stack.c
@@ -177,8 +177,8 @@ void get_the_rest_of_the_number_from_user(char *val, operation_location *op) {
   char c[2];
   c[1] = 0;
   if (isdigit(val[0])) {
+    int i = 0;
     while(!kbhit()) {
-      int i = 0;
       printf("%c", val[i]);
       ++i;
       c[0] = getCharacter();
